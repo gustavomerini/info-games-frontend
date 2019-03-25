4c67f867218b42332e611c6d86380e165b07324a
merini <gutomerini@gmail.com>
2019-03-25 18:51:52 -0300

    antd removed from project

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
 import React, { Component } from "react";
-import "antd/dist/antd.css";
-import { Layout, Menu, Breadcrumb } from "antd";
-import { Route, Switch, Link } from "react-router-dom";
+
+import { Route, Switch, NavLink } from "react-router-dom";
 
 import FreeWeek from "./containers/FreeWeek/FreeWeek";
 import Promotions from "./containers/Promotions/Promotions";
 import Home from "./containers/Home/Home";
 
-const { Header, Content, Footer } = Layout;
-
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            style={{ lineHeight: "64px" }}
-          >
-            <Menu.Item key="1">
-              <span>Home</span>
-              <Link to="/home" />
-            </Menu.Item>
-            <Menu.Item key="2">
-              <span>Free Week</span>
-              <Link to="/free-week" />
-            </Menu.Item>
-            <Menu.Item key="3">
-              <span>Promotions</span>
-              <Link to="/promotions" />
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: "0 50px", marginTop: 64 }}>
-          <Switch>
-            <Route path="/free-week" exact component={FreeWeek} />
-            <Route path="/promotions" exact component={Promotions} />
-            <Route path="/home" exact component={Home} />
-          </Switch>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Xirubot ©2019 With ❤ by Xiruteam
-        </Footer>
-      </Layout>
+      <>
+        <nav>
+          <NavLink to="/home" />
+          <NavLink to="/free-week" />
+          <NavLink to="/promotions" />
+        </nav>
+        <Switch>
+          <Route path="/free-week" exact component={FreeWeek} />
+          <Route path="/promotions" exact component={Promotions} />
+          <Route path="/home" exact component={Home} />
+        </Switch>
+      </>
     );
   }
 }

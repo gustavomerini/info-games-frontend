5dfd3004658772ccfad91c96ade7f18fa9a31643
gustavomerini <gutomerini@gmail.com>
2019-03-26 11:23:45 -0300

    Miligram init on project

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import { Route, Switch, NavLink } from "react-router-dom";
 import FreeWeek from "./containers/FreeWeek/FreeWeek";
 import Promotions from "./containers/Promotions/Promotions";
 import Home from "./containers/Home/Home";
-
+import styles from "./App.module.css";
 class App extends Component {
-  render() {
-    return (
-      <>
-        <nav>
-          <NavLink to="/home" />
-          <NavLink to="/free-week" />
-          <NavLink to="/promotions" />
-        </nav>
-        <Switch>
-          <Route path="/free-week" exact component={FreeWeek} />
-          <Route path="/promotions" exact component={Promotions} />
-          <Route path="/home" exact component={Home} />
-        </Switch>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                {/* <nav>
+                    <NavLink to="/home" />
+                    <NavLink to="/free-week" />
+                    <NavLink to="/promotions" />
+                </nav>
+                <Switch>
+                    <Route path="/free-week" exact component={FreeWeek} />
+                    <Route path="/promotions" exact component={Promotions} />
+                    <Route path="/home" exact component={Home} />
+                </Switch> */}
+            </>
+        );
+    }
 }
 
 export default App;
